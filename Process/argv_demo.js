5e2f6050ffc8ef3d40cffc8101951392d72a5ca1
vineeta <vineetavastrad@gmail.com>
2019-03-13 09:52:34 +0530

    assignment10
    process demo

---
+++ Process/argv_demo.js
@@ -0,0 +1,20 @@
+// printing names
+
+// for(var i=2;i<process.argv.length;i++)
+// {
+// console.log(String(process.argv[i]));
+// }
+
+
+//printing sum of numbers
+ var result=0;
+ for(var j=2;j<process.argv.length;j++)
+ {
+     result+=Number(process.argv[j]);
+ }
+ console.log(result);
+
+
+
+
+
